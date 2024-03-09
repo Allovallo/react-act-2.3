bf3b9b830a9dfc500358688e152a4b8f3555f477
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-09 10:48:46 +0200

    files updated

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types'
 import css from './Event.module.css'
+import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from 'react-icons/fa'
+import {formatEventStart, formatEventDuration} from 'utils'
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
+    const formattedStart = formatEventStart(start);
+    const duration = formatEventDuration(start, end)
     return (
         <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
             <p className={css.info}>
-                <i className={css.icon}></i>
+                <FaMapMarkerAlt className={css.icon} size={16} />
                 {location}
             </p>
             <p className={css.info}>
-                <i className={css.icon}></i>
+                <FaUserAlt className={css.icon} size={16}/>
                 {speaker}
             </p> 
             <p className={css.info}>
-                <i className={css.icon}></i>
-                {start}
+                <FaCalendarAlt className={css.icon} size={16}/>
+                {formattedStart}
             </p> 
             <p className={css.info}>
-                <i className={css.icon}></i>
-                Duration
+                <FaClock className={css.icon} size={16}/>
+                {duration}
             </p>        
+            <span className={`${css.chip} ${css[type]}`}>{ type}</span>
         </div>
     )
 }
